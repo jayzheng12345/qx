5f28b71a054c905dc521b928d5d9b3536fd11c68
jayzheng12345 <116085664+jayzheng12345@users.noreply.github.com>
2023-06-30 16:08:28 +0800

    更新 tian.js

--- tian.js
+++ tian.js
@@ -1,4 +1,10 @@
+[rewrite_local]
+^https:\/\/open3\.vistastory\.com\/v3\/api\/vip url script-response-body https://github.com/jayzheng12345/qx/blob/main/tian.js.js
+[mitm] 
+hostname = open3.vistastory.com
 
+//hostname :open3.vistastory.com
+//重写链接：^https:\/\/open3\.vistastory\.com\/v3\/api\/vip
 const { body, url } = $response;
 const obj = JSON.parse(body);
 if (url.includes('/get_vip_price_info')) {
