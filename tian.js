a8cfe9080a464b96058fd1ca2aabd365967e04e4
jayzheng12345 <116085664+jayzheng12345@users.noreply.github.com>
2023-06-30 15:24:43 +0800

    更新 tian.js

--- tian.js
+++ tian.js
@@ -7,6 +7,8 @@ hostname = open3.vistastory.com
 const {body, url} = $response;
 const obj = JSON.parse (body);
 if (url.includes('/get_vip_price_info')) {
-  obj.subscriptionVip.endTime = 4086238830000;
+obj.isVip = 1
+obj.expireVip = 0
+obj.subscriptionVip.endTime = 4086238830000;
 }
 $done ({body: JSON.stringify(obj)});
