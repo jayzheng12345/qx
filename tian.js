c4a0b67adcfd7d8d036c3ab2e49c4c463976ab3e
jayzheng12345 <116085664+jayzheng12345@users.noreply.github.com>
2023-06-30 15:47:09 +0800

    Update tian.js

--- tian.js
+++ tian.js
@@ -1,6 +1,7 @@
 const {body, url} = $response;
 const obj = JSON.parse (body);
-if (url.includes('/get_vip_price_info')) {
+if (url.includes('/get_vip_price_info')) 
+{
 obj.isVip = 1
 obj.expireVip = 0
 obj.subscriptionVip.endTime = 4086238830000;
