76729ce8e286a497723d6c4b9444da5d43fe38ff
jayzheng12345 <116085664+jayzheng12345@users.noreply.github.com>
2023-06-30 15:55:20 +0800

    更新 tian.js

--- tian.js
+++ tian.js
@@ -1,9 +1,9 @@
-const {body, url} = $response;
-const obj = JSON.parse (body);
-if (url.includes('/get_vip_price_info')) 
-{
-obj.isVip = 1
-obj.expireVip = 0
-obj.subscriptionVip.endTime = 4086238830000;
+
+const { body, url } = $response;
+const obj = JSON.parse(body);
+if (url.includes('/get_vip_price_info')) {
+  obj.isVip = 1;
+  obj.expireVip = 0;
+  obj.subscriptionVip.endTime = 4086238830000;
 }
-$done ({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
