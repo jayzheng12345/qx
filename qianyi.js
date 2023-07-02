48978842969ec0e4fbf88eebcead5a3d59c66410
jayzheng12345 <116085664+jayzheng12345@users.noreply.github.com>
2023-07-02 17:45:21 +0800

    更新 qianyi.js

--- qianyi.js
+++ qianyi.js
@@ -1,12 +1,18 @@
  
 [rewrite_local]
-^https:\/\/chat\.feixue666\.com\/api\/account\/info url script-response-body https://raw.githubusercontent.com/jayzheng12345/qx/main/qianyi.js
+^https:\/\/chat\.feixue666\.com\/api\/account url script-response-body https://raw.githubusercontent.com/jayzheng12345/qx/main/qianyi.js
 [mitm] 
 hostname = chat.feixue666.com
 
-var body = $response.body; 
-let obj = JSON.parse($response.body);
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse (body);
+const vip = '/info';
+if (url.indexOf(vip) != -1) {
 obj.data. remainingPoints = 999999 ;
 obj.data. totalPoints = 999999 ;
 obj.data. isDisable = 1 ;
-$done({body: JSON.stringify(obj)});
+body = JSON.stringify(obj);
+}
+$done ( {body});
